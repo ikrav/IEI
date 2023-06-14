dd8be2081002a01743420756a9c2f226215ad0c3
Ilya Kravchenko <ikrav@unl.edu>
2023-06-14 13:17:55 -0500

    Added 2D plotting example

--- ROOT_tutorial/basic_ttree_examine.py
+++ ROOT_tutorial/basic_ttree_examine.py
@@ -4,6 +4,7 @@
 #
 
 from ROOT import TH1F
+from ROOT import TH2F
 
 def examine(tree):
     # Print the tree structure
@@ -24,22 +25,36 @@ def examine(tree):
     signal_events = tree.GetEntries("event_type == 0")
     print("Total events ", total_events, " of which signal events are ", signal_events)
 
+# Define a function that draws SNR for all events
 def draw_SNR(tree):
     
     # Simplest way to draw a variable
     tree.Draw("SNR")
 
-# Set up desired histogram
-hist = TH1F("histogram","SNR for signal events",100, 0, 20)
-hist.GetXaxis().SetTitle("Signal-to-Nose")
+# Set up desired histogram for 1D
+hist1 = TH2F("histogram","SNR vs event type",100, 0, 20, 2, -0.5, 1.5)
+hist1.GetXaxis().SetTitle("Signal-to-Nose")
+
 # Define the function that draws SNR for signal only events
 def draw_signal_SNR(tree):
 
     # A more complex drawing which includes selection requirements
     # and creates a histogram of desired sort
     
-    
     # Draw from the tree with selection
     tree.Draw("SNR>>histogram","event_type == 0")
 
 
+# Set up desired histogram for 2D
+hist2 = TH2F("histogram2D","SNR vs event type",100, 0, 20, 2, -0.5, 1.5)
+hist2.GetXaxis().SetTitle("Signal-to-Nose")
+hist2.GetYaxis().SetTitle("Event type (0-signal, 1-background)")
+
+# Define the function that draws SNR for signal only events
+def draw_SNR_vs_type(tree):
+
+    # A more complex drawing which includes selection requirements
+    # and creates a histogram of desired sort
+        
+    ## Draw from the tree with selection
+    tree.Draw("event_type:SNR>>histogram2D","","colz")
